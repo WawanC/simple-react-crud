339de045a82b7c2761d1f4a3a5abc5c6890a58ae
WawanC <kurniawancristianto@gmail.com>
2021-06-14 14:06:54 +0700

    Add Table Component & Title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import logo from "./logo.svg";
+import Table from "./components/Table";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className='container-fluid d-flex align-items-center flex-column'>
+      <h1 className='p-5'>What's your favorite programming language ?</h1>
+      <Table />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+const Table = () => {
+  return (
+    <table className='table w-50 table-bordered text-center'>
+      <thead className='table-dark'>
+        <tr>
+          <th>No</th>
+          <th>Name</th>
+          <th>Language</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Kurniawan Cristianto</td>
+          <td>Javascript</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
